fcf7095a3786de3337edcb6ad7e07dd7c8ec89ca
Jun Yeop Yim <junyeobe0315@gmail.com>
2022-12-15 03:25:25 +0900

    patient data delete

--- excel_and_csv/csv_to_excel.py
+++
@@ -1,5 +0,0 @@
-import csv
-import pandas as pd
-
-file = pd.read_csv('test.csv')
-file.to_excel("test.xlsx")
--- excel_and_csv/excel_to_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def make_csv(excel_name):
-    df = pd.read_excel(excel_name+".xlsx", engine='openpyxl')
-    df.to_csv(excel_name+".csv")
-
-if __name__ == '__main__':
-    make_csv('ChrX')
